ba29aa4dffc75bf97dd7701086b9aa3a7ab95e0d
Cezar.Leahu <cezar.leahu@ness.com>
2020-04-28 16:45:27 +0300

    ACS74 ACS-107: Setup Maven project structure

---
+++ alfresco-ampalyser-analyser/src/main/java/org/alfresco/ampalyser/analyser/AnalyserApplication.java
@@ -0,0 +1,13 @@
+package org.alfresco.ampalyser.analyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyserApplication.class, args);
+	}
+
+}
