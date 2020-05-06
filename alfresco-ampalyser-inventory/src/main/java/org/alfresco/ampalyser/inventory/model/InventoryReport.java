c4b7a05a30fb10259a46c9244522bae35711abf2
DenisGabriela <Denis.Ungureanu1@ness.com>
2020-05-06 17:56:12 +0300

    ACS-183 : Implement InventoryService

---
+++ alfresco-ampalyser-inventory/src/main/java/org/alfresco/ampalyser/inventory/model/InventoryReport.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2015-2018 Alfresco Software, Ltd.  All rights reserved.
+ *
+ * License rights for this program may be obtained from Alfresco Software, Ltd.
+ * pursuant to a written agreement and any use of this program without such an
+ * agreement is prohibited.
+ */
+
+package org.alfresco.ampalyser.inventory.model;
+
+public class InventoryReport
+{
+}
---
+++ alfresco-ampalyser-inventory/src/main/java/org/alfresco/ampalyser/inventory/service/InventoryServiceImpl.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2015-2018 Alfresco Software, Ltd.  All rights reserved.
+ *
+ * License rights for this program may be obtained from Alfresco Software, Ltd.
+ * pursuant to a written agreement and any use of this program without such an
+ * agreement is prohibited.
+ */
+
+package org.alfresco.ampalyser.inventory.service;
+
+import java.io.FileInputStream;
+
+import org.alfresco.ampalyser.inventory.model.InventoryReport;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InventoryServiceImpl implements InventoryService
+{
+    @Override
+    public InventoryReport extractInventoryReport(FileInputStream war)
+    {
+        return null;
+    }
+}
