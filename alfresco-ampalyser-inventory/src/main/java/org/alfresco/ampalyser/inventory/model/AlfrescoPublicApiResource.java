f74a4fb7d1de4b1967093100c24151f884be11fb
Ancuta Morarasu <ancuta.morarasu@ness.com>
2020-05-12 17:21:12 +0300

    ACS-75 / ACS--185: Add the AlfrescoPublicApi entries to the inventory

--- alfresco-ampalyser-inventory/src/main/java/org/alfresco/ampalyser/inventory/model/AlfrescoPublicApiResource.java
+++ alfresco-ampalyser-inventory/src/main/java/org/alfresco/ampalyser/inventory/model/AlfrescoPublicApiResource.java
@@ -18,4 +18,14 @@ public class AlfrescoPublicApiResource extends AbstractResource
     {
         this.deprecated = deprecated;
     }
+
+    @Override
+    public String toString()
+    {
+        return "AlfrescoPublicApiResource{" +
+                "class=" + getName() +
+                ", deprecated=" + deprecated +
+                ", packageName='" + packageName + '\'' +
+                '}';
+    }
 }
