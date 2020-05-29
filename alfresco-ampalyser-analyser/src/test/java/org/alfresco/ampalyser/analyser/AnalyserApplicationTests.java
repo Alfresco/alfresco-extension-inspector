bcf6cfbe1584f865f8cdcd23ac02d7683776ff07
tzclucian <tzclucian@gmail.com>
2020-05-29 17:32:21 +0300

    ACS-76 : AMP-a-lyser: File overwrites    - added basic test for deserialization

--- alfresco-ampalyser-analyser/src/test/java/org/alfresco/ampalyser/analyser/AnalyserApplicationTests.java
+++ alfresco-ampalyser-analyser/src/test/java/org/alfresco/ampalyser/analyser/AnalyserApplicationTests.java
@@ -1,13 +1,34 @@
 package org.alfresco.ampalyser.analyser;
 
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+
+import org.alfresco.ampalyser.analyser.parser.InventoryParser;
+import org.alfresco.ampalyser.model.InventoryReport;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AnalyserApplicationTests {
 
+	@Autowired
+	InventoryParser jsonInventoryParser;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void basicDeserializationTest()
+	{
+		// We're using the report generated (manually) by the inventory tool on the 'test.war' resource
+		File file = new File(getClass().getClassLoader().getResource("test.inventory.json").getFile());
+
+		InventoryReport inventoryReport = jsonInventoryParser.parseReport(file.getAbsolutePath());
+
+		assertNotNull(inventoryReport);
 	}
 
 }
