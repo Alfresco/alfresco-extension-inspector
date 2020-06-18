14f9e2b2aa94b345aadae8db786d1f7281debe86
gherghelasR <gherglelas.roxana@gmail.com>
2020-06-18 10:32:08 +0300

    ACS-315: add tests for run analyserCommand

---
+++ alfresco-ampalyser-test/src/main/java/org/alfresco/ampalyser/command/CommandInventoryImpl.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2015-2020 Alfresco Software, Ltd.  All rights reserved.
+ *
+ * License rights for this program may be obtained from Alfresco Software, Ltd.
+ * pursuant to a written agreement and any use of this program without such an
+ * agreement is prohibited.
+ */
+
+package org.alfresco.ampalyser.command;
+
+import org.alfresco.ampalyser.models.CommandOutput;
+import org.alfresco.ampalyser.models.InventoryCommand;
+
+public class CommandInventoryImpl implements Command
+{
+        private CommandReceiver commReceiver;
+        private InventoryCommand inventoryCommand;
+
+        public CommandInventoryImpl(CommandReceiver commReceiver, InventoryCommand inventoryCommand)
+        {
+                this.commReceiver = commReceiver;
+                this.inventoryCommand = inventoryCommand;
+        }
+
+        @Override
+        public CommandOutput execute()
+        {
+                return commReceiver.runInventoryCmd(inventoryCommand);
+        }
+}
---
+++ alfresco-ampalyser-test/src/test/java/org/alfresco/ampalyser/integration/tests/AnalyserCommandTests.java
@@ -0,0 +1,50 @@
+package org.alfresco.ampalyser.integration.tests;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.alfresco.ampalyser.AmpalyserClient;
+import org.alfresco.ampalyser.models.CommandOutput;
+import org.alfresco.ampalyser.util.AppConfig;
+import org.alfresco.ampalyser.util.TestResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+@ContextConfiguration(classes = AppConfig.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public class AnalyserCommandTests extends AbstractTestNGSpringContextTests
+{
+        @Autowired
+        private AmpalyserClient client;
+
+        private CommandOutput cmdOut;
+
+        @Test
+        public void runCommandWithOutput()
+        {
+                String ampResourcePath = TestResource.getTestResourcePath("alfresco-mm-repo-1.4.1-808.amp");
+                String version = "6.1.1";
+                List<String> cmdOptions = List.of(ampResourcePath, "--target=" + version);
+
+                // Generate new analyser report
+                cmdOut = client.runAnalyserCommand(cmdOptions);
+                System.out.println(cmdOut.getOutput());
+
+                assertEquals(cmdOut.getExitCode(), 0);
+
+        }
+
+        @Test
+        public void runCommandWithExitCodeError()
+        {
+                cmdOut = client.runAnalyserCommand(Collections.emptyList());
+                assertEquals(cmdOut.getExitCode(), 1);
+                assertTrue(cmdOut.containsMessage("Missing extension file."));
+        }
+}
