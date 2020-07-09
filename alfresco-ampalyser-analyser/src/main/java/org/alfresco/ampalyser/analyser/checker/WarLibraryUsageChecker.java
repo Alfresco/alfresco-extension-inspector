9fe65bc1dca2f5d8f627c7761df992944a5ec7f4
Cezar.Leahu <cezar.leahu@ness.com>
2020-07-09 18:34:48 +0300

    ACS-80: Update checker

--- alfresco-ampalyser-analyser/src/main/java/org/alfresco/ampalyser/analyser/checker/WarLibraryUsageChecker.java
+++ alfresco-ampalyser-analyser/src/main/java/org/alfresco/ampalyser/analyser/checker/WarLibraryUsageChecker.java
@@ -22,12 +22,16 @@ import org.alfresco.ampalyser.analyser.service.ConfigService;
 import org.alfresco.ampalyser.analyser.service.ExtensionResourceInfoService;
 import org.alfresco.ampalyser.model.InventoryReport;
 import org.alfresco.ampalyser.model.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WarLibraryUsageChecker implements Checker
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WarLibraryUsageChecker.class);
+
     @Autowired
     private ConfigService configService;
     @Autowired
@@ -36,36 +40,38 @@ public class WarLibraryUsageChecker implements Checker
     @Override
     public Stream<Conflict> processInternal(final InventoryReport warInventory, final String alfrescoVersion)
     {
-        final Map<String, Set<Resource>> extensionClassesByName = extensionResourceInfoService.retrieveClassResourcesByName();
-        final Set<String> extensionDependencies = extensionResourceInfoService.retrieveAllDependencies();
+        final Set<String> allExtensionDependencies = extensionResourceInfoService.retrieveAllDependencies();
 
-        // Iterate through the WAR classpath elements and keep the ones that could be dependencies of the extension
-        final Set<String> dependenciesInWar = warInventory
+        // Iterate through the WAR classpath elements and keep the ones that could be dependencies of the extension.
+        // We keep this intermediate data structure (Set), so that we don't hash the entire War inventory
+        final Set<String> classesInWar = warInventory
             .getResources().getOrDefault(CLASSPATH_ELEMENT, emptyList())
             .stream()
             .map(Resource::getId)
             .filter(s -> s.endsWith(".class"))
-            .filter(s -> !s.startsWith("/org/alfresco/"))
-            .map(s -> s.substring(1)) //.replaceAll("/", ".")) todo?
-            .filter(extensionDependencies::contains)
+            .filter(s -> !s.startsWith("/org/alfresco/")) // strip any Alfresco Classes
+            .filter(allExtensionDependencies::contains) // only if the WAR entry could be a dependency of the extension
             .collect(toUnmodifiableSet());
 
+        final Map<String, Set<Resource>> extensionClassesById = extensionResourceInfoService.retrieveClasspathElementsById();
+
+        // now we can go back through the AMP dependencies and
         return extensionResourceInfoService
             .retrieveDependenciesPerClass()
             .entrySet()
             .stream()
+            // map to (class_name -> dependencies_only_present_int_the_WAR)
             .map(e -> new SimpleEntry<>(
                 e.getKey(),
                 e.getValue()
                  .stream()
-                 // filter dependencies included in the extension
-                 .filter(d -> !extensionClassesByName.containsKey(d))
-                 .filter(dependenciesInWar::contains)
+                 .filter(d -> !extensionClassesById.containsKey(d)) // dependencies not provided in the extension
+                 .filter(classesInWar::contains) // dependencies provided by the WAR
                  .collect(toUnmodifiableSet())
             ))
             .filter(e -> !e.getValue().isEmpty())
-            .flatMap(e -> extensionClassesByName
-                .get(e.getKey())
+            .flatMap(e -> extensionClassesById
+                .get(e.getKey()) // a class can be provided by multiple jars, hence multiple conflicts
                 .stream()
                 .map(r -> new WarLibraryUsageConflict(
                     r,
