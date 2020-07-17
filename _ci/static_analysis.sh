57c886a8751ce10a7060d304b145893623d168c9
tzclucian <tzclucian@gmail.com>
2020-07-17 14:55:32 +0300

    ACS-311 : AMP-a-lyser: Add veracode scans

--- _ci/static_analysis.sh
+++ _ci/static_analysis.sh
@@ -6,16 +6,14 @@ set -vex
 pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 
 # Run in a sandbox for every branch, run normally on master
-[ "${TRAVIS_BRANCH}" != "master" ] && RUN_IN_SANDBOX="-sandboxname AIS_Analysis" || RUN_IN_SANDBOX=""
+[ "${TRAVIS_BRANCH}" != "master" ] && RUN_IN_SANDBOX="-sandboxname Amp-a-lyser" || RUN_IN_SANDBOX=""
 
 java -jar vosp-api-wrappers-java-$VERACODE_WRAPPER_VERSION.jar -vid $VERACODE_API_ID \
-     -vkey $VERACODE_API_KEY -action uploadandscan -appname "Intelligence Service" \
+     -vkey $VERACODE_API_KEY -action uploadandscan -appname "Amp-a-lyser" \
      ${RUN_IN_SANDBOX} -createprofile false \
-     -filepath alfresco-ai-analysis-model/target/alfresco-ai-analysis-model-*.jar \
-     alfresco-ai-analysis-shared-services/target/alfresco-ai-analysis-shared-services-*.jar \
-     alfresco-ai-image-analysis/target/alfresco-ai-image-analysis-*.jar \
-     alfresco-ai-image-text-analysis/target/alfresco-ai-image-text-analysis-*.jar \
-     alfresco-ai-text-analysis/target/alfresco-ai-text-analysis-*.jar \
+     -filepath alfresco-ampalyser-analyser/target/alfresco-ampalyser-analyser-*.jar \
+     alfresco-ampalyser-commons/target/alfresco-ampalyser-commons-*.jar \
+     alfresco-ampalyser-inventory/target/alfresco-ampalyser-inventory-*.jar \
      -version "$TRAVIS_JOB_ID - $TRAVIS_JOB_NUMBER" -scantimeout 3600
 
 popd
