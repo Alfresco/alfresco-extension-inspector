937bb60f8924e109dd53d877f1f8d1638bec5abc
tzclucian <tzclucian@gmail.com>
2020-07-27 17:27:55 +0300

    ACS-444 : Amp-a-lyser: wording & tweaks    - added table summary for printer

--- alfresco-ampalyser-analyser/src/main/java/org/alfresco/ampalyser/analyser/service/AnalyserOutputService.java
+++ alfresco-ampalyser-analyser/src/main/java/org/alfresco/ampalyser/analyser/service/AnalyserOutputService.java
@@ -10,7 +10,6 @@ package org.alfresco.ampalyser.analyser.service;
 
 import static java.util.Comparator.comparing;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -20,10 +19,11 @@ import org.alfresco.ampalyser.analyser.result.Conflict;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.shell.table.ArrayTableModel;
+import org.springframework.shell.table.BorderStyle;
+import org.springframework.shell.table.TableBuilder;
+import org.springframework.shell.table.TableModel;
 import org.springframework.stereotype.Service;
-
-import dnl.utils.text.table.TextTable;
-import dnl.utils.text.table.csv.CsvTableModel;
 
 @Service
 public class AnalyserOutputService
@@ -37,44 +37,7 @@ public class AnalyserOutputService
 
     public void print(final Map<Conflict.Type, Map<String, Set<Conflict>>> conflictPerTypeAndResourceId)
     {
-        int conflictsTotal = 0;
-
-        StringBuilder csv = new StringBuilder();
-        csv.append("Type,Total").append(System.lineSeparator());
-
-        for (Map.Entry<Conflict.Type, Map<String, Set<Conflict>>> perType : conflictPerTypeAndResourceId.entrySet())
-        {
-            int conflictsPerType = 0;
-            for (Map.Entry<String, Set<Conflict>> perVersion : perType.getValue().entrySet())
-            {
-                conflictsPerType += perVersion.getValue().size();
-            }
-            csv.append(perType.getKey().toString()).append(",").append(conflictsPerType).append(System.lineSeparator());
-            conflictsTotal += conflictsPerType;
-        }
-
-        System.out.println("================");
-        System.out.println("REPORT SUMMARY");
-        System.out.println("================");
-
-        if (conflictsTotal > 0)
-        {
-            try
-            {
-                System.out.println("Across the provided target versions, the following number of conflicts have been found:");
-                new TextTable(new CsvTableModel(csv.toString())).printTable();
-                System.out.println();
-            }
-            catch (IOException e)
-            {
-                LOGGER.warn("Failed to summarize the output for the requested analysis.", e);
-            }
-        }
-        else {
-            System.out.println("Across the provided target versions, no conflicts have been found.");
-        }
-
-
+        printSummary(conflictPerTypeAndResourceId);
         printers
             .stream()
             .sorted(comparing(ConflictPrinter::getConflictType))
@@ -86,6 +49,49 @@ public class AnalyserOutputService
         if (!configService.isVerboseOutput())
         {
             System.out.println("(use option --verbose for version details)");
+        }
+    }
+
+    private void printSummary(Map<Conflict.Type, Map<String, Set<Conflict>>> conflictPerTypeAndResourceId)
+    {
+        String[][] data = new String[6][2];
+        data[0][0] = "Type";
+        data[0][1] = "Total";
+
+        int row = 1;
+
+        int conflictsTotal = 0;
+        for (Map.Entry<Conflict.Type, Map<String, Set<Conflict>>> perType : conflictPerTypeAndResourceId.entrySet())
+        {
+            int conflictsPerType = 0;
+            for (Map.Entry<String, Set<Conflict>> perVersion : perType.getValue().entrySet())
+            {
+                conflictsPerType += perVersion.getValue().size();
+            }
+
+            data[row][0] = perType.getKey().toString();
+            data[row][1] = String.valueOf(conflictsPerType);
+            row++;
+
+            conflictsTotal += conflictsPerType;
+        }
+
+        System.out.println("================");
+        System.out.println("REPORT SUMMARY");
+        System.out.println("================");
+
+        if (conflictsTotal > 0)
+        {
+            System.out.println("Across the provided target versions, the following number of conflicts have been found:");
+            TableModel tableModel = new ArrayTableModel(data);
+            TableBuilder tableBuilder = new TableBuilder(tableModel);
+            tableBuilder.addInnerBorder(BorderStyle.fancy_light);
+            tableBuilder.addHeaderBorder(BorderStyle.fancy_double);
+            System.out.println(tableBuilder.build().render(255));
+            System.out.println();
+        }
+        else {
+            System.out.println("Across the provided target versions, no conflicts have been found.");
         }
     }
 }
