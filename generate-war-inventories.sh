0b628917762b0d9f7b13885c82897c1963469427
CezarLeahu <35226487+CezarLeahu@users.noreply.github.com>
2020-07-29 18:08:18 +0300

    ACS-455: Update bundled inventory reports (#61)

--- generate-war-inventories.sh
+++ generate-war-inventories.sh
@@ -1,29 +1,17 @@
 #!/usr/bin/env bash
 # On MacOS run the script through the Homebrew bash version: "$ /usr/local/bin/bash generate-war-inventory.sh"
 
-pushd "$(dirname "${BASH_SOURCE[0]}")"
+pushd "$(dirname "${BASH_SOURCE[0]}")" || exit 3
 
-# Wars and their S3 locations:
+if [ ! -d "wars" ] ; then
+    echo "Missing \"$(pwd)/wars/\" directory. Exiting..."
+    exit 1
+fi
 
-declare -A WAR_S3_LOCATIONS=(
-["6.2.0"]="https://s3-eu-west-1.amazonaws.com/eu.dl.alfresco.com/release/enterprise/ACS/6.2/6.2.0/5/alfresco.war"
-["6.2.1"]="https://s3-eu-west-1.amazonaws.com/eu.dl.alfresco.com/release/enterprise/ACS/6.2/6.2.1/2500/alfresco.war"
-)
-
-echo "S3 WARs:"
-for k in "${!WAR_S3_LOCATIONS[@]}" ; do
-  echo "${k} -> ${WAR_S3_LOCATIONS[$k]}"
-done
-
-mkdir -p "wars/"
-
-# Won't work without AWS Authorization header
-#echo "Downloading S3 WARs:"
-#for k in "${!WAR_S3_LOCATIONS[@]}"; do
-#  if [ ! -f "wars/${k}.war" ]; then
-#    curl -s -S "${WAR_S3_LOCATIONS[$k]}" -o "wars/${k}.war"
-#  fi
-#done
+if ! ls wars/*.war &>/dev/null ; then
+    echo "Directory \"$(pwd)/wars/\" contains no wars. Exiting..."
+    exit 2
+fi
 
 echo "Rebuilding project..."
 mvn clean install -DskipTests &>/dev/null || exit 1
@@ -41,8 +29,10 @@ for file in wars/*.war ; do
   version=${file#"wars/"}
   version=${version%".war"}
   echo "Generating report for version [${version}]..."
-  java -jar "${JAR_EXECUTABLE}" "${file}" --o "${INVENTORY_DIR}/${version}.json"
+  java -jar "${JAR_EXECUTABLE}" "${file}" --o="${INVENTORY_DIR}/${version}.json"
+
+  #mv -f "${version}.inventory.json" "${INVENTORY_DIR}/${version}.json"
 done
 
-popd
+popd || exit 4
 exit 0
