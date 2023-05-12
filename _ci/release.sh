d378c493d851f667fdafa87cc26aa78af7f357d9
kshah <kavit.shah@globallogic.com>
2023-05-12 11:14:55 +0530

    Modified Release Config.

--- _ci/release.sh
+++ _ci/release.sh
@@ -10,8 +10,12 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 
 # Travis CI runner work on DETACHED HEAD, so we need to checkout the release branch
 git checkout -B "${BRANCH_NAME}"
+# Get the latest changes on branch
+git pull
 
-git config user.email "build@alfresco.com"
+# Add email to link commits to user
+git config user.email "${GIT_EMAIL}"
+git config user.name "${GIT_USERNAME}"
 
 # Run the release plugin - with "[skip ci]" in the release commit message
 
@@ -21,7 +25,7 @@ mvn -B \
     "-Darguments=-DskipTests -Dmaven.javadoc.skip -Dadditionalparam=-Xdoclint:none -Dbuildnumber=${BUILD_NUMBER}" \
     -DscmCommentPrefix="[maven-release-plugin][skip ci] " \
     release:clean release:prepare release:perform \
-    -Dusername=alfresco-build \
+    -Dusername="${GIT_USERNAME}" \
     -Dpassword=${GIT_PASSWORD}
 
 popd
